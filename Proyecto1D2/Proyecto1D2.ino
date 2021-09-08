08a1b7097e0d4cea2086c3bd1fde66e2d3eeda15
alessandropaiz1 <alessandropch@gmail.com>
2021-09-07 22:46:39 -0600

    Crear la función de configurar display
    Se creó la función para configurarlo, configurar los casos y el punto, respectivamente

--- Proyecto1D2/Proyecto1D2.ino
+++ Proyecto1D2/Proyecto1D2.ino
@@ -49,6 +49,9 @@ void configurarLedVerde(void);
 void configurarLedAmarilla(void);
 void configurarLedRoja(void);
 void configurarBinicio(void);
+void configurarDisplay(uint8_t A, uint8_t B, uint8_t C, uint8_t D, uint8_t E, uint8_t F, uint8_t G, uint8_t dP);
+void desplegar7Seg(uint8_t digito); 
+void desplegarPunto(boolean punto);
 void EMAADC(void);
 void semaforo(void);
 
